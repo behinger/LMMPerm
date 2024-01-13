7aeac6f0c9b2fc6616fd89cb4a6c151eccb14153
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2024-01-13 15:10:38 +0000

    format; reml default

--- src/sim_parameters.jl
+++ src/sim_parameters.jl
@@ -2,113 +2,106 @@ using DrWatson
 using StatsModels # for @formula
 using MixedModels # for zercorr
 function defaultFormulas()
-    f1 =  @formula(dv ~ 1 + condition  + (1|subj))
-    f2 =  @formula(dv ~ 1 + condition  + zerocorr(1+condition|subj))
-    f3 =  @formula(dv ~ 1 + condition  + (1+condition|subj))
-    f4 =  @formula(dv ~ 1 + condition  + (1+condition|subj) + (1+condition|item))
-    return f1,f2,f3,f4
+    f1 = @formula(dv ~ 1 + condition + (1 | subj))
+    f2 = @formula(dv ~ 1 + condition + zerocorr(1 + condition | subj))
+    f3 = @formula(dv ~ 1 + condition + (1 + condition | subj))
+    f4 = @formula(dv ~ 1 + condition + (1 + condition | subj) + (1 + condition | item))
+    return f1, f2, f3, f4
 end
 
 function defaultParameters()
-    _,_,f3,_ = defaultFormulas()
+    _, _, f3, _ = defaultFormulas()
     return Dict(
         "nRep" => 5000,
         "statsMethod" => ["permutation"], #"waldsT","pBoot","LRT","KenwardRoger"
         "f" => [f3],#f1,f2,f4 #int-only, zerocorr, max, max+item
-        "σs" => [[[1., 1.],[0.,0.]]],
-        "β" => [[0., 0.]],
+        "σs" => [[[1.0, 1.0], [0.0, 0.0]]],
+        "β" => [[0.0, 0.0]],
         "errorDistribution" => ["normal",],#"tdist","skewed"
         "imbalance" => [nothing],#"subject","trial",
         "nSubject" => [30],
         "nItemsPerCondition" => [30],
-        "nPerm"=> [@onlyif(("statsMethod"=="permutation")|("statsMethod"=="pBoot"),1000)],
-        "blupMethod" => [@onlyif("statsMethod"=="permutation",ranef)],#olsranef,
-        "residualMethod" => [@onlyif("statsMethod"=="permutation",:shuffle)],# @onlyif("statsMethod"=="permutation",:signflip)
-        "inflationMethod" => [@onlyif("statsMethod" == "permutation",MixedModelsPermutations.inflation_factor)],#, "noScaling"],
-        "σ" => 1.,
+        "nPerm" => [@onlyif(("statsMethod" == "permutation") | ("statsMethod" == "pBoot"), 1000)],
+        "blupMethod" => [@onlyif("statsMethod" == "permutation", ranef)],#olsranef,
+        "residualMethod" => [@onlyif("statsMethod" == "permutation", :shuffle)],# @onlyif("statsMethod"=="permutation",:signflip)
+        "inflationMethod" => [@onlyif("statsMethod" == "permutation", MixedModelsPermutations.inflation_factor)],#, "noScaling"],
+        "σ" => 1.0,
+        "reml" => false,
     )
 end
 function getParamList(task)
-    f1,f2,f3,f4 = defaultFormulas()
+    f1, f2, f3, f4 = defaultFormulas()
 
-   default = defaultParameters()
+    default = defaultParameters()
     if task == 1
         paramList = Dict(
-            "f" => [f1,f2,f3,f4],
-            "σs" => [@onlyif("f"!= f4, [[1., 0.],[0.,0.]]),
-                     @onlyif("f"!= f4, [[1., 1.],[0.,0.]]),  
-                     @onlyif("f"!= f4, [[1., 4.],[0.,0.]]),
-                     @onlyif("f"!= f4, [[4., 1.],[0.,0.]]),
-        
-                     @onlyif("f"== f4, [[1., 1.], [1., 0.]]),
-                     @onlyif("f"== f4, [[1., 1.], [1., 1.]]),
-                     @onlyif("f"== f4, [[1., 1.], [1., 4.]])],
-
-            "blupMethod" => [ranef,@onlyif("f"!= f4,olsranef)],
-            "inflationMethod" => [MixedModelsPermutations.inflation_factor,"noScaling"],
-            "residualMethod" => [:signflip,:shuffle],            
+            "f" => [f1, f2, f3, f4],
+            "σs" => [@onlyif("f" != f4, [[1.0, 0.0], [0.0, 0.0]]),
+                @onlyif("f" != f4, [[1.0, 1.0], [0.0, 0.0]]),
+                @onlyif("f" != f4, [[1.0, 4.0], [0.0, 0.0]]),
+                @onlyif("f" != f4, [[4.0, 1.0], [0.0, 0.0]]), @onlyif("f" == f4, [[1.0, 1.0], [1.0, 0.0]]),
+                @onlyif("f" == f4, [[1.0, 1.0], [1.0, 1.0]]),
+                @onlyif("f" == f4, [[1.0, 1.0], [1.0, 4.0]])], "blupMethod" => [ranef, @onlyif("f" != f4, olsranef)],
+            "inflationMethod" => [MixedModelsPermutations.inflation_factor, "noScaling"],
+            "residualMethod" => [:signflip, :shuffle],
         )
-        elseif task == 2
+    elseif task == 2
         #----
         # H1 test
         # test some special cases of \sigmas / f1 
         paramList = Dict(
-            "f" => [f1,f3,f4],
-            "σs" => [@onlyif("f"== f1, [[1., 0.], [0.,0.]]),
-                     @onlyif("f"== f3, [[1., 1.], [0.,0.]]),  
-                     @onlyif("f"== f3, [[1., 4.], [0.,0.]]),
-                     @onlyif("f"== f4, [[1., 1.], [1., 1.]]),
-                     ],
-            "β" =>[[0., 0.],[0., 0.1],[0., 0.2],[0., .3],[0., 0.5]],
+            "f" => [f1, f3, f4],
+            "σs" => [@onlyif("f" == f1, [[1.0, 0.0], [0.0, 0.0]]),
+                @onlyif("f" == f3, [[1.0, 1.0], [0.0, 0.0]]),
+                @onlyif("f" == f3, [[1.0, 4.0], [0.0, 0.0]]),
+                @onlyif("f" == f4, [[1.0, 1.0], [1.0, 1.0]]),
+            ],
+            "β" => [[0.0, 0.0], [0.0, 0.1], [0.0, 0.2], [0.0, 0.3], [0.0, 0.5]],
         )
-        
-        elseif task == 3
+
+    elseif task == 3
         #----
         # Power calculations
         paramList = Dict(
-            "statsMethod" => ["waldsT","pBoot","permutation","LRT","KenwardRoger"], # if this is "missing" we run permutation for backward compatibility
-            "β" => [[0., 0.],[0., 0.1],[0., 0.2],[0., .3],[0., 0.5]]        
+            "statsMethod" => ["waldsT", "pBoot", "permutation", "LRT", "KenwardRoger"], # if this is "missing" we run permutation for backward compatibility
+            "β" => [[0.0, 0.0], [0.0, 0.1], [0.0, 0.2], [0.0, 0.3], [0.0, 0.5]]
         )
-        
-        
-        elseif task == 4
+
+
+    elseif task == 4
         #-----
         # Varying N
         paramList = Dict(
-            "statsMethod" => ["waldsT","pBoot","permutation","LRT","KenwardRoger"], # if this is "missing" we run permutation for backward compatibility
-            "errorDistribution" => ["normal","tdist","skewed","skewed_40"],
-            "β" => [[0., 0.],[0., 0.3]],
-            
-            "blupMethod" => [@onlyif("statsMethod"=="permutation",ranef),
-                             @onlyif("statsMethod"=="permutation",olsranef)],
-            "nSubject" => [4,10,30],    
-            "nItemsPerCondition" => [2,10,30,50],
+            "statsMethod" => ["waldsT", "pBoot", "permutation", "LRT", "KenwardRoger"], # if this is "missing" we run permutation for backward compatibility
+            "errorDistribution" => ["normal", "tdist", "skewed", "skewed_40"],
+            "β" => [[0.0, 0.0], [0.0, 0.3]], "blupMethod" => [@onlyif("statsMethod" == "permutation", ranef),
+                @onlyif("statsMethod" == "permutation", olsranef)],
+            "nSubject" => [4, 10, 30],
+            "nItemsPerCondition" => [2, 10, 30, 50],
         )
-        
-        elseif task == 5
-            #-----
-            # Errordistributions + balancing
-            paramList = Dict(
-                "statsMethod" => ["waldsT","pBoot","permutation","LRT","KenwardRoger"], # if this is "missing" we run permutation for backward compatibility
-                "errorDistribution" => ["normal","tdist","skewed"],
-                "imbalance" => ["subject","trial"],
-                "σs" => [[[1., 1.],[0.,0.]]],
-                "residualMethod" => [@onlyif("statsMethod"=="permutation",:shuffle),@onlyif("statsMethod"=="permutation",:signflip)],
-                "nSubject" => [10,30],
-            )
-        elseif task == 6
-              # Varying N
-            paramList = Dict(
-                "statsMethod" => ["waldsT","pBoot","permutation"],
-                "β" => [[0., 0.],[0., 0.3]],
-                
-                "blupMethod" => [@onlyif("statsMethod"=="permutation",ranef),
-                                @onlyif("statsMethod"=="permutation",olsranef)],
-                "nSubject" => [4,10,30],    
-                "nItemsPerCondition" => [2,10,30,50],
-                "reml" => [true]
-            )
-                
-        end
-        return merge(default,paramList)
+
+    elseif task == 5
+        #-----
+        # Errordistributions + balancing
+        paramList = Dict(
+            "statsMethod" => ["waldsT", "pBoot", "permutation", "LRT", "KenwardRoger"], # if this is "missing" we run permutation for backward compatibility
+            "errorDistribution" => ["normal", "tdist", "skewed"],
+            "imbalance" => ["subject", "trial"],
+            "σs" => [[[1.0, 1.0], [0.0, 0.0]]],
+            "residualMethod" => [@onlyif("statsMethod" == "permutation", :shuffle), @onlyif("statsMethod" == "permutation", :signflip)],
+            "nSubject" => [10, 30],
+        )
+    elseif task == 6
+        # Varying N
+        paramList = Dict(
+            "statsMethod" => ["waldsT", "pBoot", "permutation"],
+            "β" => [[0.0, 0.0], [0.0, 0.3]], "blupMethod" => [@onlyif("statsMethod" == "permutation", ranef),
+                @onlyif("statsMethod" == "permutation", olsranef)],
+            "nSubject" => [4, 10, 30],
+            "nItemsPerCondition" => [2, 10, 30, 50],
+            "reml" => [true]
+        )
+
+    end
+    return merge(default, paramList)
 end
--- src/sim_results.jl
+++ src/sim_results.jl
@@ -3,140 +3,144 @@ using SharedArrays
 #using CategorialArrays
 
 function read_results(folder)
-    c = collect_results(folder);
+    c = collect_results(folder)
     return read_results(c)
-    
+
 end
 function read_results!(folder)
-    c = collect_results!(folder;update=true);
-    return read_results(c);
+    c = collect_results!(folder; update=true)
+    return read_results(c)
 end
-    function read_results(c::DataFrame)
+function read_results(c::DataFrame)
 
-    
 
-	# calculate p < 0.05
-	c = c[.!ismissing.(c.results),:]
+
+    # calculate p < 0.05
+    c = c[.!ismissing.(c.results), :]
     for r = 1:nrow(c)
-        x = c[r,:]     
-        if "side"  ∉ names(x.results)
-            x.results[!,"side"] .= "twosided"
+        x = c[r, :]
+        if "side" ∉ names(x.results)
+            x.results[!, "side"] .= "twosided"
         end
     end
 
-c = @rtransform(c,:p = combine(groupby(:results,["coefname","test", "side"]),Symbol("pval") => x->mean(x .<= .05)))
-c = @rtransform(c,:w = combine(groupby(:results,["coefname","test", "side"]),Symbol("warnings") => (x->sum(x[1]))=>Symbol("maxtime"),Symbol("warnings") => (x->sum(x[2]))=>Symbol("NLoptRoundoff"),
-Symbol("warnings") => (x->sum(x[3]))=>Symbol("maxfval")))
+    c = @rtransform(c, :p = combine(groupby(:results, ["coefname", "test", "side"]), Symbol("pval") => x -> mean(x .<= 0.05)))
 
-	# move result table to own columns
+    c = @rtransform(c, :r = combine(groupby(:results, ["coefname", "test", "side"]), Symbol("runtime") => (x -> quantile(x, 0.05)) => Symbol("r_low"),
+        Symbol("runtime") => (x -> quantile(x, 0.5)) => Symbol("r_median"),
+        Symbol("runtime") => (x -> quantile(x, 0.95)) => Symbol("r_high")))
 
-c = @rtransform(c,:coefname = :p.coefname,:test=:p.test,:pval=:p.pval_function,:side=:p.side,:warn_maxtime = :w.maxtime,:warn_nlopt = :w.NLoptRoundoff,:warn_maxfval = :w.maxfval)
+    c = @rtransform(c, :w = combine(groupby(:results, ["coefname", "test", "side"]), Symbol("warnings") => (x -> sum(x[1])) => Symbol("maxtime"), Symbol("warnings") => (x -> sum(x[2])) => Symbol("NLoptRoundoff"),
+        Symbol("warnings") => (x -> sum(x[3])) => Symbol("maxfval")))
 
-c = flatten(c,[:coefname,:test,:pval,:side,:warn_maxfval,:warn_nlopt,:warn_maxtime])[:,Not([:w,:p,:results])]
+    # move result table to own columns
 
+    c = @rtransform(c, :coefname = :p.coefname, :test = :p.test, :pval = :p.pval_function, :side = :p.side,
+        :warn_maxtime = :w.maxtime, :warn_nlopt = :w.NLoptRoundoff, :warn_maxfval = :w.maxfval, :runtime_median = :r.r_median, :runtime_low = :r.r_low, :runtime_high = :r.r_high)
 
-
-
-	# move result table to own columns
-
-	# replace missing statsmethod with permutation
-	c.statsMethod[ismissing.(c.statsMethod)] .= "permutation"
-	#disallowmissing!(c.statsMethod)
-	c.pval[c.statsMethod .== "pBoot"] = 1 .-c.pval[c.statsMethod .== "pBoot"]
-
-	# rename formulas
-	@rtransform!(c, :f_simple = simpleFormula(:f))
-	@transform!(c,:f_simple = categorical(:f_simple,levels=["1|s","1+a||s","1+a|s","1+a|s + 1+a|i"]))
+    c = flatten(c, [:coefname, :test, :pval, :side, :warn_maxfval, :warn_nlopt, :warn_maxtime, :runtime_median, :runtime_low, :runtime_high])[:, Not([:r, :w, :p, :results])]
 
 
-	# rename σs
-	@rtransform!(c,:σs_simple = simpleσs(:σs))
-	@transform!(c,:σs_simple=categorical(:σs_simple,levels=["1|s","1+1*a|s","4+1*a|s","1+4*a|s","1+1*a|s+1|i","1+1*a|s+1+1*a|i","1+1*a|s+1+4*a|i"]))
 
 
-	# replace weird JLD names
-	jldreplace = x->replace(string(x),
-	"JLD2.ReconstructedTypes"=>"",
-	"##"=>"",
-	"var"=>"",
-	"."=>"",
-	"MixedModels"=>"",
-	"Permutations"=>"",
-	"#"=>"",
-	r"[0-9\(\)]*"=>"",
-	
-	)
+    # move result table to own columns
 
-	c.blupMethod = jldreplace.(c.blupMethod)
-	c.inflationMethod = jldreplace.(c.inflationMethod)
-return c
+    # replace missing statsmethod with permutation
+    c.statsMethod[ismissing.(c.statsMethod)] .= "permutation"
+    #disallowmissing!(c.statsMethod)
+    c.pval[c.statsMethod.=="pBoot"] = 1 .- c.pval[c.statsMethod.=="pBoot"]
+
+    # rename formulas
+    @rtransform!(c, :f_simple = simpleFormula(:f))
+    @transform!(c, :f_simple = categorical(:f_simple, levels=["1|s", "1+a||s", "1+a|s", "1+a|s + 1+a|i"]))
+
+
+    # rename σs
+    @rtransform!(c, :σs_simple = simpleσs(:σs))
+    @transform!(c, :σs_simple = categorical(:σs_simple, levels=["1|s", "1+1*a|s", "4+1*a|s", "1+4*a|s", "1+1*a|s+1|i", "1+1*a|s+1+1*a|i", "1+1*a|s+1+4*a|i"]))
+
+
+    # replace weird JLD names
+    jldreplace = x -> replace(string(x),
+        "JLD2.ReconstructedTypes" => "",
+        "##" => "",
+        "var" => "",
+        "." => "",
+        "MixedModels" => "",
+        "Permutations" => "",
+        "#" => "",
+        r"[0-9\(\)]*" => "",)
+
+    c.blupMethod = jldreplace.(c.blupMethod)
+    c.inflationMethod = jldreplace.(c.inflationMethod)
+    return c
 end
 
 
-simpleFormula(f::FormulaTerm) = simpleFormula(string(f.rhs)|>x->replace(x," "=>""))
+simpleFormula(f::FormulaTerm) = simpleFormula(string(f.rhs) |> x -> replace(x, " " => ""))
 function simpleFormula(f::AbstractString)
-	return f == "1+condition+:(1|subj)" ? "1|s" : 
-f == "1+condition+:((1+condition)|subj)" ? "1+a|s" : 
-f == "1+condition+:(zerocorr((1+condition)|subj))" ? "1+a||s" : 
-f == "1+condition+:((1+condition)|subj)+:((1+condition)|item)" ? "1+a|s + 1+a|i" : "can't parse $f"
+    return f == "1+condition+:(1|subj)" ? "1|s" :
+           f == "1+condition+:((1+condition)|subj)" ? "1+a|s" :
+           f == "1+condition+:(zerocorr((1+condition)|subj))" ? "1+a||s" :
+           f == "1+condition+:((1+condition)|subj)+:((1+condition)|item)" ? "1+a|s + 1+a|i" : "can't parse $f"
 end
 
 
 function simpleσs(σs)
-return σs == [[1., 0],[0., 0]] ? "1|s" : 
-σs == [[1., 1],[0., 0]] ? "1+1*a|s" : 
-σs == [[1., 1]] ? "1+1*a|s" : 
-σs == [[4., 1],[0., 0]] ? "4+1*a|s" : 
-σs == [[1., 1],[1., 0]] ? "1+1*a|s+1|i" : 
-σs == [[1., 1],[1., 4]] ? "1+1*a|s+1+4*a|i" : 
-σs == [[1., 1],[1., 1]] ? "1+1*a|s+1+1*a|i" : 
-σs == [[1., 4],[0., 0]] ? "1+4*a|s" : "undefined"
+    return σs == [[1.0, 0], [0.0, 0]] ? "1|s" :
+           σs == [[1.0, 1], [0.0, 0]] ? "1+1*a|s" :
+           σs == [[1.0, 1]] ? "1+1*a|s" :
+           σs == [[4.0, 1], [0.0, 0]] ? "4+1*a|s" :
+           σs == [[1.0, 1], [1.0, 0]] ? "1+1*a|s+1|i" :
+           σs == [[1.0, 1], [1.0, 4]] ? "1+1*a|s+1+4*a|i" :
+           σs == [[1.0, 1], [1.0, 1]] ? "1+1*a|s+1+1*a|i" :
+           σs == [[1.0, 4], [0.0, 0]] ? "1+4*a|s" : "undefined"
 
 end
 
 function simpleDefaultParameters()
 
-	def = DataFrame(Ref(dict_list(defaultParameters())[1]))
- @transform!(def,@byrow(:f_simple = simpleFormula(:f)),
-	 			 @byrow(:σs_simple = simpleσs(:σs)))
+    def = DataFrame(Ref(dict_list(defaultParameters())[1]))
+    @transform!(def, @byrow(:f_simple = simpleFormula(:f)),
+        @byrow(:σs_simple = simpleσs(:σs)))
 
-				 @rtransform!(def,:residualMethod = string(:residualMethod),:blupMethod = string(:blupMethod),:inflationMethod = string(:inflationMethod);renamecols=false)
-				 
-				 return def
+    @rtransform!(def, :residualMethod = string(:residualMethod), :blupMethod = string(:blupMethod), :inflationMethod = string(:inflationMethod); renamecols=false)
+
+    return def
 end
 
 
-function subselectDF(a;skip=[],def = simpleDefaultParameters(),debug=false,clip=0.1)
-	def = deepcopy(def)
-	def.coefname .="condition: B"
-	def.test .= "z"
-	def.side .=:twosided
+function subselectDF(a; skip=[], def=simpleDefaultParameters(), debug=false, clip=0.1)
+    def = deepcopy(def)
+    def.coefname .= "condition: B"
+    def.test .= "z"
+    def.side .= :twosided
 
-	skipAll = hcat(skip...,["test","σs","f"]...)
-	debug ? @show(skipAll) : ""
-	onstring = setdiff(names(def),skipAll)
-	
-	#ix = Vector{Bool}(true,nrow(c))
-	ix = fill(true,nrow(a))
-	
-	for k in onstring
-		v = def[1,k]
-		ix_l = isequal.(a[!,k],Ref(v))  .||  isequal.(a[!,k],"missing")  .||  isequal.(a[!,k],missing) 
+    skipAll = hcat(skip..., ["test", "σs", "f"]...)
+    debug ? @show(skipAll) : ""
+    onstring = setdiff(names(def), skipAll)
 
-		ix = ix .&& ix_l
-		debug ? println("$k - $v | $(sum(ix_l)) | remaining: $(sum(ix))") : ""
-		@assert sum(ix_l) > 0 "$k,$v - $(unique(a[:,k]))"
-	end
+    #ix = Vector{Bool}(true,nrow(c))
+    ix = fill(true, nrow(a))
 
-debug ? @show(sum(ix)) : ""
-	
-	a =  select(a[ix,:],Not(:f))
+    for k in onstring
+        v = def[1, k]
+        ix_l = isequal.(a[!, k], Ref(v)) .|| isequal.(a[!, k], "missing") .|| isequal.(a[!, k], missing)
 
-	#@show unique(a.test)
-	if !("test" ∈ skip)
-		@rsubset!(a,:test !== "β")
-	end
-	#@show unique(a.test)
-	a =  @transform(a,@byrow :pval=((:pval>clip) ? clip : :pval))	
-	return a
+        ix = ix .&& ix_l
+        debug ? println("$k - $v | $(sum(ix_l)) | remaining: $(sum(ix))") : ""
+        @assert sum(ix_l) > 0 "$k,$v - $(unique(a[:,k]))"
+    end
+
+    debug ? @show(sum(ix)) : ""
+
+    a = select(a[ix, :], Not(:f))
+
+    #@show unique(a.test)
+    if !("test" ∈ skip)
+        @rsubset!(a, :test !== "β")
+    end
+    #@show unique(a.test)
+    a = @transform(a, @byrow :pval = ((:pval > clip) ? clip : :pval))
+    return a
 end
