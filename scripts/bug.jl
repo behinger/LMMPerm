29c714595523044bb69e7a4994d7b93e7bd1c5fb
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2024-01-13 14:37:57 +0000

    removed old files

--- scripts/bug.jl
+++
@@ -1,6 +0,0 @@
-using FileIO,JLD2, MixedModelsPermutations
-g = FileIO.load("data/bug_threading2.jld2")
-g  = FileIO.load("/store/users/ehinger/unfoldjl_dev/data/bug_threading/bug_threading2.jld2")
-H0 = coef(g["mm"])
-H0[2] = 0
-perm = permutation(16000,g["mm"];β=H0,blup_method=olsranef,use_threads=false); 
